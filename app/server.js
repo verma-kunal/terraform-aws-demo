f86712fd9cf331e85491e5627f0f7d14c151498c
Kunal Verma <kverma.main@gmail.com>
2024-08-22 23:35:02 +0530

    update: port statement

--- app/server.js
+++ app/server.js
@@ -44,5 +44,5 @@ db.connect((err) => {
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
